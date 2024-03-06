755132da9195387217a119f4006e0a81a00fdc5c
nakyeonko3 <badaclock@gmail.com>
2024-03-06 20:41:14 +0900

    first commit

---
+++ src/main/java/com/group/companytimeclockapp/CompanyTimeclockAppApplication.java
@@ -0,0 +1,13 @@
+package com.group.companytimeclockapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyTimeclockAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyTimeclockAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group/companytimeclockapp/CompanyTimeclockAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.companytimeclockapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyTimeclockAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
