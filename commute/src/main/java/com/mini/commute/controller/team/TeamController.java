7fee12b2256dee8ac8f3d34c88ce74722838cfe2
jooda00 <dragon2000a@gmail.com>
2024-03-06 22:14:48 +0900

    feat: team 등록, 목록 조회 기능

---
+++ commute/src/main/java/com/mini/commute/controller/team/TeamController.java
@@ -0,0 +1,35 @@
+package com.mini.commute.controller.team;
+
+import com.mini.commute.common.ResponseData;
+import com.mini.commute.dto.team.TeamRequest;
+import com.mini.commute.dto.team.TeamResponse;
+import com.mini.commute.service.team.TeamService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TeamController {
+    private final TeamService teamService;
+
+    public TeamController(TeamService teamService) {
+        this.teamService = teamService;
+    }
+
+    @PostMapping("/teams")
+    public ResponseEntity<ResponseData> saveTeam(@RequestBody TeamRequest request) {
+        teamService.saveTeam(request);
+        ResponseData responseData = new ResponseData(200, request);
+        return new ResponseEntity<>(responseData, HttpStatus.OK);
+    }
+
+    @GetMapping("/teams")
+    public ResponseEntity<ResponseData> getAllTeams() {
+        List<TeamResponse> teamResponses = teamService.getTeams();
+        ResponseData responseData = new ResponseData(200, teamResponses);
+        return new ResponseEntity<>(responseData, HttpStatus.OK);
+    }
+}
---
+++ commute/src/main/java/com/mini/commute/service/team/TeamService.java
@@ -0,0 +1,43 @@
+package com.mini.commute.service.team;
+
+import com.mini.commute.dto.team.TeamRequest;
+import com.mini.commute.dto.team.TeamResponse;
+import com.mini.commute.entity.team.Team;
+import com.mini.commute.repository.employee.EmployeeRepository;
+import com.mini.commute.repository.team.TeamRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional(readOnly = true)
+public class TeamService {
+    private final TeamRepository teamRepository;
+    private final EmployeeRepository employeeRepository;
+
+    public TeamService(TeamRepository teamRepository, EmployeeRepository employeeRepository) {
+        this.teamRepository = teamRepository;
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Transactional
+    public void saveTeam(TeamRequest request) {
+        teamRepository.save(new Team(request.getName()));
+    }
+
+    public List<TeamResponse> getTeams() {
+        return teamRepository.findAll()
+                .stream()
+                .map(TeamResponse::new)
+                .collect(Collectors.toList());
+    }
+
+    public List<TeamResponse> getTeamsByFetch() {
+        return teamRepository.findAllJPQLFetch()
+                .stream()
+                .map(TeamResponse::new)
+                .collect(Collectors.toList());
+    }
+}
